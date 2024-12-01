14cf14a291350c9c2c8df0d8a15ffb11d0bd0540
Subset28 <122221745+Subset28@users.noreply.github.com>
2024-12-01 13:15:30 -0500

    Add files via upload

---
+++ Ancel/find_network_adapter_path.bat
@@ -0,0 +1,6 @@
+@echo off
+
+for /f %%n in ('Reg query "HKLM\SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}" /v "*SpeedDuplex" /s ^| findstr  "HKEY"') do (
+echo %%n
+)
+pause
